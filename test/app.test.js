9862daa7031f592fe6bb7bf9a5f8caba410ccb8d
Karthi-8055 <vvce23cse0280@vvce.ac.in>
2025-11-29 13:53:56 +0530

    version 6

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app'); // adjust path to your main app file
+const app = require('../vvce-lost-found'); // adjust path to your main app file
 
 describe('Basic endpoint tests', () => {
   it('GET / should return 200', async () => {
